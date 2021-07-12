8012c01f9999e42d3682430c8b95130d5591cfac
rod-rodriguezf <rod.rodriguezf@outlook.com>
2021-07-12 05:36:45 -0400

    confirmo

---
+++ tallerMecanico/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import index, trabajos, registrarse, iniciar, mantencion1, mantencion2, mantencion3, mantencion4, mantencion5, mantencion6
+
+urlpatterns = [
+    path('', index, name='INDEX'),
+    path('trabajos/', trabajos, name='TRABAJOS'),
+    path('registrarse/', registrarse, name='REGISTRARSE'),
+    path('iniciar/', iniciar, name='INICIAR'),
+    path('mantencion1/', mantencion1, name='MANTENCION1'),
+    path('mantencion2/', mantencion2, name='MANTENCION2'),
+    path('mantencion3/', mantencion3, name='MANTENCION3'),
+    path('mantencion4/', mantencion4, name='MANTENCION4'),
+    path('mantencion5/', mantencion5, name='MANTENCION5'),
+    path('mantencion6/', mantencion6, name='MANTENCION6'),
+    
+]
---
+++ tallerMecanico/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def trabajos(request):
+    return render(request, "trabajos.html")
+
+def registrarse(request):
+    return render(request, "registrarse.html")
+
+def iniciar(request):
+    return render(request, "iniciarSesion.html")
+
+def mantencion1(request):
+    return render(request, "mantencion1.html")
+
+def mantencion2(request):
+    return render(request, "mantencion2.html")
+
+def mantencion3(request):
+    return render(request, "mantencion3.html")
+
+def mantencion4(request):
+    return render(request, "mantencion4.html")
+
+def mantencion5(request):
+    return render(request, "mantencion5.html")
+
+def mantencion6(request):
+    return render(request, "mantencion6.html")
